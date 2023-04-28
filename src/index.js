822a11ad4aa3b71d6e0ef20b41a0b5ada3475748
lidrail35 <lidrail35@mail.ru>
2023-04-28 22:08:45 +0300

    feat: add line of buttons to keyboard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.html';
 import './style.css';
+import keyData from './keysdata';
 
 const { body } = document;
 
@@ -7,10 +8,11 @@ class Keyboard {
   constructor(width, height) {
     this.width = width || 75;
     this.height = height || 25 * 5;
-    this.workSpace = null;
     this.viewField = this.createViewField();
     this.keyBoard = this.createKeyBoard();
+    this.workSpace = this.createWorkSpaсe();
     this.lang = 'en';
+    this.keyLines = [];
   }
 
   createViewField() {
@@ -19,9 +21,44 @@ class Keyboard {
     return this.viewField;
   }
 
+  createButton(btn) {
+    const button = document.createElement('div');
+    const langOne = document.createElement('span');
+    const langTwo = document.createElement('span');
+
+    button.className = 'keyboard-button';
+    langOne.className = 'lang-one';
+    langTwo.className = 'lang-two';
+
+    langOne.textContent = btn.en;
+    langTwo.textContent = btn.ru;
+
+    button.append(langOne);
+    button.append(langTwo);
+
+    return button;
+  }
+
+  createKeyLines(arr) {
+    const keyLine = document.createElement('div');
+    keyLine.className = 'key-lines';
+
+    const arrB = arr.map((x) => {
+      x.pointer = this.createButton(x);
+      x.pointer.className = 'key';
+      return x;
+    });
+
+    arrB.map((x) => keyLine.append(x.pointer));
+
+    return keyLine;
+  }
+
   createKeyBoard() {
     this.keyBoard = document.createElement('div');
     this.keyBoard.className = 'keyboard';
+    this.keyBoard.append(this.createKeyLines(keyData));
+
     return this.keyBoard;
   }
 
@@ -39,4 +76,4 @@ class Keyboard {
 }
 
 const board = new Keyboard();
-board.createWorkSpaсe();
+// board.createWorkSpaсe();
---
+++ src/keysdata.js
@@ -0,0 +1,15 @@
+const keyData = [
+  {
+    key: 'KeyZ', ru: 'я', en: 'z', size: 1, pointer: null,
+  },
+  {
+    key: 'KeyX', ru: 'ч', en: 'x', size: 1, pointer: null,
+  },
+  {
+    key: 'KeyC', ru: 'с', en: 'c', size: 1, pointer: null,
+  },
+  {
+    key: 'KeyV', ru: 'м', en: 'v', size: 1, pointer: null,
+  },
+];
+export default keyData;
