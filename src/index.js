2d33bd66b0edfe3f52c2223d278cf9cfa1aa3a2d
lidrail35 <lidrail35@mail.ru>
2023-05-01 02:38:56 +0300

    feat: add functional for buttons CapsLock and shift

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class Keyboard {
     this.altLeft = false;
     this.crtl = false;
     this.alt = false;
+    this.CapsLock = false;
     this.workSpace = null;
     this.viewField = this.createViewField();
     this.keyBoard = this.createKeyBoard();
@@ -38,6 +39,10 @@ class Keyboard {
         x.pointer.children[1].classList.toggle('not-active');
       });
     }
+    const flg = ((!this.shift && this.CapsLock) || (this.shift && !this.CapsLock));
+    keyData
+      .filter((x) => (Object.prototype.hasOwnProperty.call(x, 'en')))
+      .map((x) => x.pointer.classList.toggle('caps-lock', flg));
   }
 
   writeSymbolToViewField(keyButton) {
@@ -45,8 +50,7 @@ class Keyboard {
     const str = this.viewField.value;
     const strLenght = this.viewField.value.length;
     const currStart = this.viewField.selectionStart;
-    this.redrawButton();
-    // if this.altKey && this.shiftKey
+
     // if (keyButton.includes('Left') || keyButton.includes('Right')) return;
     const pressedKey = keyData.filter((x) => x.key === keyButton);
     if (pressedKey[0].key === 'ArrowLeft') {
@@ -74,12 +78,20 @@ class Keyboard {
       this.viewField.selectionStart = currStart + 1;
       this.viewField.selectionEnd = currStart + 1;
     }
+    if (pressedKey[0].key === 'CapsLock') {
+      this.CapsLock = !this.CapsLock;
+      keyData.find((x) => x.key === 'CapsLock').pointer.classList.toggle('fix-key', this.CapsLock);
+      keyData
+        .filter((x) => (Object.prototype.hasOwnProperty.call(x, 'en')))
+        .map((x) => x.pointer.classList.toggle('caps-lock'));
+    }
     if (Object.prototype.hasOwnProperty.call(pressedKey[0], 'content')) {
       charToOut = pressedKey[0].content;
       this.viewField.value += charToOut;
     }
     if (Object.prototype.hasOwnProperty.call(pressedKey[0], 'en')) {
-      charToOut = this.shift ? pressedKey[0][this.lang].toUpperCase()
+      charToOut = (!this.shift && this.CapsLock) || (this.shift && !this.CapsLock)
+        ? pressedKey[0][this.lang].toUpperCase()
         : pressedKey[0][this.lang];
       if (strLenght === currStart) this.viewField.value += charToOut;
       else {
@@ -103,19 +115,23 @@ class Keyboard {
       langOne.className = 'lang-one';
       langTwo.className = 'lang-two not-active';
 
-      langOne.textContent = btn.en.toUpperCase();
-      langTwo.textContent = btn.ru.toUpperCase();
+      langOne.textContent = btn.en;
+      langTwo.textContent = btn.ru;
 
       button.append(langOne);
       button.append(langTwo);
     }
     button.addEventListener('mousedown', (e) => {
       e.target.closest('.key').classList.toggle('press', true);
+      if ((btn.key === 'ShiftLeft') || (btn.key === 'ShiftRight')) this.shift = true;
+      this.redrawButton();
       this.writeSymbolToViewField(btn.key);
       // this.viewField.textContent += btn.en;
     });
     button.addEventListener('mouseup', (e) => {
       e.target.closest('.key').classList.toggle('press', false);
+      if ((btn.key === 'ShiftLeft') || (btn.key === 'ShiftRight')) this.shift = false;
+      this.redrawButton();
     });
 
     return button;
@@ -147,6 +163,7 @@ class Keyboard {
   addListener() {
     document.addEventListener('keydown', (e) => {
       e.preventDefault();
+      if ((e.code === 'ShiftLeft') && e.repeat) return;
       console.log(`down ${e.key}, ${e.code}, ${e.shiftKey}`);
       const pressedKey = keyData.filter((x) => x.key === e.code.toString());
       if (pressedKey.length !== 0) pressedKey[0].pointer.classList.toggle('press', true);
@@ -155,10 +172,10 @@ class Keyboard {
       this.crtl = e.ctrlKey;
       if (e.code === 'ShiftLeft') this.shiftLeft = true;
       if (e.code === 'AltLeft') this.altLeft = true;
+      this.redrawButton();
       this.writeSymbolToViewField(e.code);
     });
     document.addEventListener('keyup', (e) => {
-      console.log(`up  ${e.key}, ${e.code}`);
       const pressedKey = keyData.filter((x) => x.key === e.code.toString());
       if (pressedKey.length !== 0) pressedKey[0].pointer.classList.toggle('press', false);
       this.shift = e.shiftKey;
@@ -166,6 +183,7 @@ class Keyboard {
       this.crtl = e.ctrlKey;
       if (e.code === 'ShiftLeft') this.shiftLeft = false;
       if (e.code === 'AltLeft') this.altLeft = false;
+      this.redrawButton();
     });
   }
 
